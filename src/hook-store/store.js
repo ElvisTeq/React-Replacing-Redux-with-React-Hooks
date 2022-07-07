1bb454d3b7d9bd326c3fc7bc7f762d2bd5173415
elvis <cenelvis@gmail.com>
2022-07-06 21:03:11 -0400

    Creating Custom Hook

---
+++ src/hook-store/store.js
@@ -0,0 +1,37 @@
+import { useEffect, useState } from "react";
+
+let globalState = {}; // Data
+let listeners = []; // contains ".set" functions for the "globalState"
+let actions = {}; // functions
+
+export const useStore = () => {
+  const setState = useState(globalState)[1]; // [1] => [state, setState] (to get "setState" function)
+
+  const dispatch = (actionIdenfifier) => {
+    const newState = actions[actionIdenfifier](globalState);
+    globalState = { ...globalState, ...newState };
+
+    for (const listener of listeners) {
+      listener(globalState); // listener => contain setState() function
+    }
+  };
+
+  useEffect(() => {
+    listeners.push(setState); // Add setState Function to "listeners"
+
+    return () => {
+      listeners = listeners.filter((li) => li !== setState); // Remove setState Function from "listeners"
+    };
+  }, [setState]); // "set" always runs once
+
+  return [globalState, dispatch];
+};
+
+// Return Updated Data & Actions (functions)
+export const initStore = (userActions, initialState) => {
+  if (initialState) {
+    globalState = { ...globalState, ...initialState };
+  }
+
+  actions = { ...actions, ...userActions };
+};
